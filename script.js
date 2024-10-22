37579445649575853b6bd411e1893d62d8387809
Gustavo <gustavotimbo6@gmail.com>
2024-10-22 19:17:29 -0300

    search tab created

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let suggestionsBox = document.getElementById("searchbarsuggestions");
+let searchbar = document.getElementById("searchbar");
+let searchbarInputField = document.getElementById("searchbarinputfield");
+searchbarInputField.value = "";
+let searchbarCloser = document.getElementById("searchbarcloser");
 let sidebar = document.getElementById("sidebar");
 let sidebarOpener = document.getElementById("sidebaropener");
 let sidebarOpenerArrow = document.getElementById("sidebaropenerarrow");
 let sidebarCloserMobile = document.getElementById("sidebarcloser_mobile");
-let suggestionsBox = document.getElementById("searchbarsuggestions");
-let searchbar = document.getElementById("searchbar");
+let mainBody = document.getElementById("main-body");
 let tabs = document.getElementsByClassName("tab");
 let fields = document.getElementsByClassName("field");
 let subjects = document.getElementsByClassName("subject");
@@ -11,8 +15,9 @@ let subjects = document.getElementsByClassName("subject");
 // get the path
 let mainFolder = "";
 let fullpath = window.location.pathname;
-if(fullpath.includes("subject")) {
-    mainFolder = "../../";
+let depth = Array.from(fullpath.slice(fullpath.indexOf("QuestionsGenerator")).matchAll("/")).length - 1;
+for(index = 0; index < depth; index++) {
+    mainFolder += "../";
 }
 // get the current tab
 currentTab = "";
@@ -25,9 +30,10 @@ for(let index = 6; index < fullpath.length; index++) {
 
 // remember stuff that were done in other tabs
 if(localStorage.getItem("sidebarIsOpen") == "true") {
-    sidebar.classList.add('open');
-    sidebarOpener.classList.add('open');
-    sidebarOpenerArrow.src = mainFolder + "assets/leftarrow.png";
+    sidebar.classList.add("sidebarisopen");
+    sidebarOpener.classList.add("sidebarisopen");
+    mainBody.classList.add("sidebarisopen")
+    sidebarOpenerArrow.style = "transform: rotate(90deg);";
     
     if(window.innerWidth <= 720) {
         sidebarCloserMobile.style.display = "block";
@@ -42,7 +48,7 @@ for(let fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
         for(let index = 0; index < fieldSubjects.length; index++) {
             fieldSubjects[index].style.display = "flex";
         }
-        arrow.src = mainFolder + "assets/downarrow.png";
+        arrow.style = "transform: rotate(0deg);";
     }
 }
 
@@ -69,53 +75,100 @@ function getFieldSubjects(field) {
 
 
 
-// search bar function
-function showSuggestions(str) {
-    str = str.toLowerCase();
-    if(str.length == 0) {
-        suggestionsBox.innerHTML = "";
-        searchbar.style = "border-bottom-left-radius: 21.6px; border-bottom-right-radius: 21.6px;"
+// search bar functions
+function getSuggestions(inputValue) {
+    let suggestions = [];
+    if(inputValue.length != 0) {
+        let numberOfSuggestions = 0;
+        let suggestionsStr = [];
+        for(let index = 0; index < subjects.length; index++) { // find results with matching start
+            if(numberOfSuggestions == 5) {
+                break;
+            }
+            if(subjects[index].dataset.subject.includes(inputValue) && subjects[index].dataset.subject.startsWith(inputValue)) {
+                suggestions.push(subjects[index]);
+                suggestionsStr.push(subjects[index].innerText.toLowerCase());
+                numberOfSuggestions++;
+            }
+        }
+        for(let index = 0; index < subjects.length; index++) { // other results
+            if(numberOfSuggestions == 5) {
+                break;
+            }
+            if(subjects[index].dataset.subject.includes(inputValue) && !(suggestionsStr.includes(subjects[index].dataset.subject))) {
+                suggestions.push(subjects[index]);
+                numberOfSuggestions++;
+            }
+        }
+    }
+    return suggestions;
+}
+let roundedBorder = "border-bottom-left-radius: 21.6px; border-bottom-right-radius: 21.6px;";
+let notRoundedBorder = "border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;";
+function showSuggestions(event) {
+    inputValue = searchbarInputField.value.toLowerCase();
+    let suggestions = getSuggestions(inputValue);
+    if(inputValue.length == 0) {
+        clearSuggestions();
         return;
     }
     else {
         let numberOfSuggestions = 0;
         suggestionsBox.innerHTML = "";
-        for(let index = 0; index < subjects.length; index++) {
-            if(subjects[index].dataset.subject.includes(str)) {
-                suggestionsBox.innerHTML += `<div class="suggestion"><a class="suggestion-link" href="${mainFolder}subjects/${subjects[index].dataset.field}/${subjects[index].dataset.tab}.html">${capitalizeFirstLetter(subjects[index].dataset.subject)}</a></div>`;
-                numberOfSuggestions++;
-            }
+        for(let index = 0; index < suggestions.length; index++) {
+            suggestionsBox.innerHTML += `<div class="suggestion"><a class="suggestion-link" href="${mainFolder}subjects/${suggestions[index].dataset.field}/${suggestions[index].dataset.tab}.html">${capitalizeFirstLetter(suggestions[index].dataset.subject)}</a></div>`;
         }
-        if(numberOfSuggestions == 0) {
-            suggestionsBox.innerHTML += `<div class="suggestion"><a style="cursor: default">Page not found</a></div>`;
+        if(suggestions.length == 0) {
+            suggestionsBox.innerHTML += `<div class="suggestion"><a class="suggestion-link" style="cursor: default">Page not found</a></div>`;
         }
-        let suggestions = document.getElementsByClassName("suggestion");
-        searchbar.style = "border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;"
-        suggestions[suggestions.length-1].style = "border-bottom-left-radius: 21.6px; border-bottom-right-radius: 21.6px;"
-        document.getElementsByClassName("suggestion-link")[numberOfSuggestions-1].style = "border-bottom-left-radius: 21.6px; border-bottom-right-radius: 21.6px;";
+        suggestions = document.getElementsByClassName("suggestion");
+        searchbar.style = notRoundedBorder;
+        searchbarCloser.style = "display: flex;";
+        suggestions[suggestions.length-1].style = roundedBorder
+        document.getElementsByClassName("suggestion-link")[suggestions.length-1].style = roundedBorder;
+        if(event.key == "Enter") {
+            search();
+        }
+    }
+}
+function clearSuggestions() {
+    suggestionsBox.innerHTML = "";
+    searchbar.style = roundedBorder;
+    searchbarCloser.style = "display: none;";
+    searchbarInputField.value = "";
+}
+function search() {
+    let inputValue = searchbarInputField.value;
+    let suggestions = getSuggestions(inputValue);
+    if(inputValue != "") {
+        window.open("miscpages/searchresults.html", "_self");
     }
 }
 
 // open and close sidebar function
 function openCloseSidebar() {
-    sidebar.classList.toggle("open");
-    sidebar.style.transition = "left 0.4s";
-    sidebarOpener.classList.toggle("open");
-    sidebarOpener.style.transition = "left 0.4s";
-    localStorage.setItem("sidebarIsOpen", sidebar.classList.contains("open"));
+    sidebar.classList.toggle("sidebarisopen");
+    sidebar.style = "transition: left 0.4s;";
+    sidebarOpener.classList.toggle("sidebarisopen");
+    sidebarOpener.style = "transition: left 0.4s;";
+    mainBody.classList.toggle("sidebarisopen");
 
     if(localStorage.getItem("sidebarIsOpen") == "true") {
-        sidebarOpenerArrow.src = mainFolder + "assets/rightarrow.png";
+        sidebarOpenerArrow.style = "transform: rotate(-90deg);";
+        mainBody.style = "animation-name: close";
         if(window.innerWidth <= 720) {
             sidebarCloserMobile.style.display = "none";
         }
     }
     else {
-        sidebarOpenerArrow.src = mainFolder + "assets/leftarrow.png";
+        sidebarOpenerArrow.style = "transform: rotate(90deg);";
+        mainBody.style = "animation-name: open";
         if(window.innerWidth <= 720) {
             sidebarCloserMobile.style.display = "block";
         }
     }
+
+    localStorage.setItem("sidebarIsOpen", sidebar.classList.contains("sidebarisopen"));
 }
 
 // open and close fields function
@@ -128,14 +181,16 @@ function openCloseFields(fieldIndex) {
         for(let index = 0; index < fieldSubjects.length; index++) {
             fieldSubjects[index].style.display = "none";
         }
-        arrow.src = mainFolder + "assets/rightarrow.png";
+        arrow.style = "transform: rotate(-90deg);";
         localStorage.setItem(`field${field.dataset.field}IsOpen`, false);
     }
     else {
         for(let index = 0; index < fieldSubjects.length; index++) {
             fieldSubjects[index].style.display = "flex";
         }
-        arrow.src = mainFolder + "assets/downarrow.png";
+        arrow.style = "transform: rotate(0deg);";
         localStorage.setItem(`field${field.dataset.field}IsOpen`, true);
     }
 }
+
+searchbarInputField.addEventListener("keyup", showSuggestions, false)
